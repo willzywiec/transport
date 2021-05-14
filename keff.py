f0112260a9a70450ed9965efadebe4dfd95576b1
Will Zywiec <30445407+wzywiec@users.noreply.github.com>
2021-05-13 21:38:22 -0700

    add files and refactor scripts

--- keff.py
+++ keff.py
@@ -10,32 +10,32 @@ from os.path import isfile, join
 
 myPath = getcwd()
 files = [f for f in listdir(myPath) if (isfile(join(myPath,f)) and '.o' in f)]
-files = [f for f in files if 'slurm' not in f] # handles Slurm output
+files = [f for f in files if 'slurm' not in f] # ignore Slurm output
 files.sort()
 
 f_out = open('all_keffs.csv', 'w')
 
-# search file, line by line, for 'Average Multiplication' and 'final result'
+# search file, line by line, for 'Average Multiplication' or 'final result'
 for file in files:
-    f = open(file)
-    if 'Average Multiplication' not in open(file).read() or 'final result' not in open(file).read():
-        str = file
-        str += ', error\n'
-        f_out.write(str)
-    else:
-        for line in f:
-            # COG
-            if 'Average Multiplication' in line:
-                str = file
-                for num in line.split('=')[1].split():
-                    str += ', ' +  num
-            if 'Standard Deviation' in line:
-                for num in line.split('=')[1].split():
-                    str += ', ' +  num
-                f_out.write(str + '\n') # save each line as 'filename, num1, num2, ...'
-            # MCNP
-            if 'final result' in line:
-                str = file
-                str += ', ' + line.split(' ')[16]
-                str += ', ' + line.split(' ')[25] + '\n'
-                f_out.write(str)
+	f = open(file)
+	if 'Average Multiplication' not in open(file).read() or 'final result' not in open(file).read():
+		str = file
+		str += ', error\n'
+		f_out.write(str)
+	else:
+		for line in f:
+			# COG
+			if 'Average Multiplication' in line:
+				str = file
+				for num in line.split('=')[1].split():
+					str += ', ' +  num
+			if 'Standard Deviation' in line:
+				for num in line.split('=')[1].split():
+					str += ', ' +  num
+				f_out.write(str + '\n') # save each line as 'filename, num1, num2, ...'
+			# MCNP
+			if 'final result' in line:
+				str = file
+				str += ', ' + line.split(' ')[16]
+				str += ', ' + line.split(' ')[25] + '\n'
+				f_out.write(str)
